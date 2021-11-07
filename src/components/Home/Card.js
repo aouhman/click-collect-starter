058befa84e6fc133a68eac939345135f7e290b3d
aouhman <abdessamad.ouhman@gmail.com>
2021-11-07 22:50:10 +0100

    récupérer et afficher les produits avec gql de apollo client

--- src/components/Home/Card.js
+++ src/components/Home/Card.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import { Link } from "react-router-dom";
 
-function Card() {
+function Card({id,name,price}) {
     return(
         <div className="col-6 col-md-6 col-lg-4 mb-3">
             <div className="card h-100 border-0">
                 <div className="card-img-top">
-                <img src={`images/men_1.png`} className="img-fluid mx-auto d-block" alt="Card image cap" />
+                <img src={`images/${id}.png`} className="img-fluid mx-auto d-block" alt="Card image cap" />
                 </div>
                 <div className="card-body text-center">
                 <h4 className="card-title">
@@ -14,10 +14,10 @@ function Card() {
                           pathname: "/product",
                           props: { product: 'men_1' }
                       }}  
-                      className=" font-weight-bold text-dark text-uppercase small">PRODUCT NAME</Link>
+                      className=" font-weight-bold text-dark text-uppercase small">{name}</Link>
                 </h4>
                 <h5 className="card-price small text-warning">
-                    €49
+                    ${price}
                 </h5>
                 </div>
             </div>
--- src/components/Home/Gallery.js
+++ src/components/Home/Gallery.js
@@ -1,4 +1,8 @@
 import React from 'react'
+import {useQuery} from "@apollo/client";
+import {GET_PRODUCTS} from "../../lib/queries";
+import Cart from "../Cart";
+import Card from "./Card";
 const styles = {
     gallery : {
       height: 'calc(100vh - 120px)', 
@@ -6,12 +10,21 @@ const styles = {
     }
 }
 
-// main
+// main 48. Apollo Client Hook : useQuery
 function Gallery() {
+    const {loading,error,data} = useQuery(
+        GET_PRODUCTS,{
+            variables:{category:"women"}
+        })
+    if(loading) return <div>Loading</div>;
+    if(error) return <div>Sorry an error occurred {error}</div>
+    if(!data) return <div>No data!</div>
     return(<div className="col-md-8 order-md-2 col-lg-9">
       <div className="container-fluid" style={styles.gallery}>
         <div className="row">
-            {/* products */}
+            {data?.products.map((product)=>(
+                <Card {...product}/>
+                ))}
         </div>
       </div>
     </div>)
